8f76667e140bca78d6348378c9ccde37c805fae8
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-23 14:40:58 -0600

    Create routes for login, register, and welcome page
    Co-authored-by: Mack Halliday 
    <16658577+MackHalliday@users.noreply.github.com>
    Co-authored-by: Scott Payton 
    <33855435+scottzero@users.noreply.github.com>
    Co-authored-by: Nancy Lee 
    <24424825+nancylee713@users.noreply.github.com>
    
    Co-Authored-By: Mack Halliday <16658577+MackHalliday@users.noreply.github.com>
    Co-Authored-By: Scott Payton <33855435+scottzero@users.noreply.github.com>
    Co-Authored-By: nancylee713 <srlee713@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  get "/register", to: "users#new"
+  post "/register", to: "users#create"
 end
