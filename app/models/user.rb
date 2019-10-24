2da9bbed4c718a3b25dea59a3453e312b3dae63b
MackHalliday <mackenzie.halliday@gmail.com>
2019-10-24 13:07:32 -0600

    Add functionality to create new user if email not already used in user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
-  validates_presence_of :username, :google_token, :email
+  validates_presence_of :username, :email
+
+  def self.from_omniauth(auth)
+    where(email: auth.info.email).first_or_initialize do |user|
+      user.username = auth.info.name
+      user.email = auth.info.email
+    end
+  end
 end
