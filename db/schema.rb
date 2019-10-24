8c3ac4e8bda4ba90a074ca5f90edf41309b91021
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-24 16:45:33 -0600

    Add migration and update schema with game table
    Co-authored-by: Mack Halliday 
    <16658577+MackHalliday@users.noreply.github.com>
    Co-authored-by: Scott Payton 
    <33855435+scottzero@users.noreply.github.com>
    Co-authored-by: Nancy Lee 
    <24424825+nancylee713@users.noreply.github.com>
    
    Co-Authored-By: Mack Halliday <16658577+MackHalliday@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_23_002522) do
+ActiveRecord::Schema.define(version: 2019_10_24_223614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string "category"
+    t.string "number_of_questions"
+    t.string "difficulty"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_games_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -22,4 +30,5 @@ ActiveRecord::Schema.define(version: 2019_10_23_002522) do
     t.string "email"
   end
 
+  add_foreign_key "games", "users"
 end
