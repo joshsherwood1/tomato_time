68a3b8d0ec142b7cc5a15fc4637ae2de8e533c47
MackHalliday <mackenzie.halliday@gmail.com>
2019-10-24 13:08:30 -0600

    User can delete game from game#index

--- spec/features/users/user_can_view_their_games_spec.rb
+++ spec/features/users/user_can_view_their_games_spec.rb
@@ -1,45 +1,58 @@
 require 'rails_helper'
 
 describe 'A registered user' do
-  it 'can see all their games' do
-    user = User.create!(username: "User Name", email: "email@email.com", google_token: "token")
-    game_1 = Game.create!(custom_name: "MathTime", category: "Math", number_of_questions: "10", difficulty: "easy", user_id: user.id)
-    game_2 = Game.create!(custom_name: "SpellingTime", category: "Spelling", number_of_questions: "20", difficulty: "hard", user_id: user.id)
-    game_3 = Game.create!(custom_name: "FoodTime", category: "Food", number_of_questions: "30", difficulty: "easy", user_id: user.id)
+  before :each do
+    @user = User.create!(username: "User Name", email: "email@email.com", google_token: "token")
+    @game_1 = Game.create!(custom_name: "MathTime", category: "Math", number_of_questions: "10", difficulty: "easy", user_id: @user.id)
+    @game_2 = Game.create!(custom_name: "SpellingTime", category: "Spelling", number_of_questions: "20", difficulty: "hard", user_id: @user.id)
+    @game_3 = Game.create!(custom_name: "FoodTime", category: "Food", number_of_questions: "30", difficulty: "easy", user_id: @user.id)
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
     visit "/games"
+  end
+  it 'can see all their games' do
 
-    expect(page).to have_content("#{user.username}'s Games")
-
-    save_and_open_page
+    expect(page).to have_content("#{@user.username}'s Games")
 
     within ".games" do
-      within "#game-#{game_1.id}" do
-        expect(page).to have_content(game_1.custom_name)
-        expect(page).to have_content(game_1.category)
-        expect(page).to have_content(game_1.number_of_questions)
-        expect(page).to have_content(game_1.difficulty)
+      within "#game-#{@game_1.id}" do
+        expect(page).to have_content(@game_1.custom_name)
+        expect(page).to have_content(@game_1.category)
+        expect(page).to have_content(@game_1.number_of_questions)
+        expect(page).to have_content(@game_1.difficulty)
         expect(page).to have_button("Start")
         expect(page).to have_button("Delete")
       end
-      within "#game-#{game_2.id}" do
-        expect(page).to have_content(game_2.custom_name)
-        expect(page).to have_content(game_2.category)
-        expect(page).to have_content(game_2.number_of_questions)
-        expect(page).to have_content(game_2.difficulty)
+      within "#game-#{@game_2.id}" do
+        expect(page).to have_content(@game_2.custom_name)
+        expect(page).to have_content(@game_2.category)
+        expect(page).to have_content(@game_2.number_of_questions)
+        expect(page).to have_content(@game_2.difficulty)
         expect(page).to have_button("Start")
         expect(page).to have_button("Delete")
       end
-      within "#game-#{game_3.id}" do
-        expect(page).to have_content(game_3.custom_name)
-        expect(page).to have_content(game_3.category)
-        expect(page).to have_content(game_3.number_of_questions)
-        expect(page).to have_content(game_3.difficulty)
+      within "#game-#{@game_3.id}" do
+        expect(page).to have_content(@game_3.custom_name)
+        expect(page).to have_content(@game_3.category)
+        expect(page).to have_content(@game_3.number_of_questions)
+        expect(page).to have_content(@game_3.difficulty)
         expect(page).to have_button("Start")
         expect(page).to have_button("Delete")
       end
     end
+  end
+
+  it 'can delete a game' do
+    within "#game-#{@game_3.id}" do
+      click_button("Delete")
+    end
+
+    expect(current_path).to eq("/games")
+    expect(page).to_not have_css("#game-#{@game_3.id}")
+    expect(page).to_not have_content("#{@game_3.custom_name}")
+
+    expect(page).to have_css("#game-#{@game_1.id}")
+    expect(page).to have_css("#game-#{@game_2.id}")
   end
 end
